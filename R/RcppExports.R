cb7c0e8f3ddd31c01e599af13211bd37deab292b
Kelly Moran <krmoran@g.clemson.edu>
2019-09-27 14:36:20 -0400

    updating Rcpp exports after rmoval of redundancy

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -9,7 +9,3 @@ sfpca_bic <- function(x, K, Omegv, sp_locsv, lamvs, alphavs) {
     .Call(`_ssJIVE_sfpca_bic`, x, K, Omegv, sp_locsv, lamvs, alphavs)
 }
 
-soft_thr <- function(input, lambda, pos, sp_locs) {
-    .Call(`_ssJIVE_soft_thr`, input, lambda, pos, sp_locs)
-}
-
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -36,25 +36,10 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// soft_thr
-arma::colvec soft_thr(arma::colvec input, double lambda, int pos, arma::rowvec sp_locs);
-RcppExport SEXP _ssJIVE_soft_thr(SEXP inputSEXP, SEXP lambdaSEXP, SEXP posSEXP, SEXP sp_locsSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::colvec >::type input(inputSEXP);
-    Rcpp::traits::input_parameter< double >::type lambda(lambdaSEXP);
-    Rcpp::traits::input_parameter< int >::type pos(posSEXP);
-    Rcpp::traits::input_parameter< arma::rowvec >::type sp_locs(sp_locsSEXP);
-    rcpp_result_gen = Rcpp::wrap(soft_thr(input, lambda, pos, sp_locs));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_ssJIVE_soft_thr", (DL_FUNC) &_ssJIVE_soft_thr, 4},
     {"_ssJIVE_sfpca_bic", (DL_FUNC) &_ssJIVE_sfpca_bic, 6},
-    {"_ssJIVE_soft_thr", (DL_FUNC) &_ssJIVE_soft_thr, 4},
     {NULL, NULL, 0}
 };
 
